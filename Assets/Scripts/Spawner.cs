de10496ff8a90f85ebb69533b75eb82d54cbac64
Santiago Abad <santiago.abad.ponce@gmail.com>
2025-12-01 18:22:39 -0800

    spawner works now

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -12,49 +12,40 @@ public class Spawner : MonoBehaviour
     public Transform maxX;
     public Transform maxZ;
 
-    private float randomX;
-    private float randomZ;
-
-
     public Transform spawnPosition;
-
     public quaternion Q;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         delayPreUpdate = delay;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float delayUpdate=delay;
-        
-        if (currentSpawns <  maxSpawns)
+        if (currentSpawns < maxSpawns)
         {
             if (delay > 0)
             {
-                delay = delay - Time.deltaTime;
+                delay -= Time.deltaTime;
             }
             else
             {
-                currentSpawns = currentSpawns + 1;
-                Instantiate(Character, spawnPosition.position,Q);
+                currentSpawns++;
+
+                GameObject enemy = Instantiate(Character, spawnPosition.position, Q);
+
+                // Assign reference so the enemy knows its spawner
+                enemy.GetComponent<EnemySpawnTracker>().spawner = this;
+
                 delay = delayPreUpdate;
             }
         }
-        
     }
-    private void selectRandomSpawnPoint ()
+
+    public void EnemyDied()
     {
-        float distanceX = transform.position.x - maxX.position.x;
-        float distanceZ = transform.position.z - maxX.position.z;
-
-        randomX = Random.Range((transform.position.x+distanceX),maxX.position.x);
-        randomX = Random.Range((transform.position.z + distanceZ), maxZ.position.z);
-        spawnPosition.position = new Vector3(randomX, spawnPosition.position.y,randomZ);
-
-        Q = quaternion.Euler(new Vector3(0, Random.Range(0, 361),0));
+        currentSpawns--;
+        if (currentSpawns < 0)
+            currentSpawns = 0;
     }
 }
