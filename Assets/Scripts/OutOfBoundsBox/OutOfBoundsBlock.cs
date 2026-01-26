10911b4fc2d2a2e5682231d3869405572bd0cce1
Ghuztavo <ghuztavo2002@gmail.com>
2026-01-25 23:06:10 -0800

    Respawn added
    Respawn that relocates the player if they fall out of bounds

---
+++ Assets/Scripts/OutOfBoundsBox/OutOfBoundsBlock.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class OutOfBoundsBlock : MonoBehaviour
+{
+    [SerializeField] private Transform playerPos;
+    [SerializeField] private float yOffset = -7f;
+    [SerializeField] private float minY = -30f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerPos == null) return;
+
+        // Desired Y position
+        float targetY = playerPos.position.y + yOffset;
+
+        // Clamp so it never goes below minY
+        targetY = Mathf.Max(targetY, minY);
+
+        // Follow player
+        transform.position = new Vector3(
+            playerPos.position.x,
+            targetY,
+            playerPos.position.z
+        );
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<OutOfBoundsRespawn>()?.RespawnPlayer();
+        }
+    }
+}
