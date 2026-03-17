53520dcb7cd266a731078df3ef152f0e2120722d
Craig Lacey <craig.lacey@gmail.com>
2026-03-17 15:51:50 -0700

    cleanup commit

---
+++ Assets/Scripts/Editors/Mesh/ProceduralLevel.cs
@@ -0,0 +1,14 @@
+using LevelGenerator;
+using UnityEngine;
+
+public class ProceduralLevel : MonoBehaviour
+{
+    [SerializeField] private MapGenerator _mapGenerator;
+    [SerializeField] private EndlessTerrain _endlessTerrain;
+
+    private void Awake()
+    {
+        
+    }
+
+}
