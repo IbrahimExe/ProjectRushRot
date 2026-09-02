6a201085392f0f3d4a0109bdf4023d5e02f0ee52
Ghuztavo <ghuztavo2002@gmail.com>
2026-09-01 17:53:27 -0700

    Changes to the player protector so it deactivates anything that is not the player, the deathwall or the ground

--- Assets/Scripts/PlayerHelpers/PlayerProtector.cs
+++ Assets/Scripts/PlayerHelpers/PlayerProtector.cs
@@ -2,42 +2,61 @@ using UnityEngine;
 
 public class PlayerProtector : MonoBehaviour
 {
-    [SerializeField] private LayerMask[] protectedLayers;
+    [Tooltip("Select all the layers that should NOT be deactivated.")]
+    [SerializeField] private LayerMask protectedLayers;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private CapsuleCollider[] capsuleColliders;
+
     void Start()
     {
-        
+        // Get all capsule colliders on this object
+        capsuleColliders = GetComponents<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        CheckAndDeactivate();
+    }
+
+    private void CheckAndDeactivate()
+    {
+        if (capsuleColliders == null || capsuleColliders.Length == 0) return;
+
+        // Use the radius of the first capsule collider (assuming they are the same size)
+        float radius = capsuleColliders[0].radius;
         
+        // Account for any scaling on the GameObject
+        float scaledRadius = radius * Mathf.Max(transform.lossyScale.x, transform.lossyScale.y, transform.lossyScale.z);
+        
+        // OverlapSphere detects ALL colliders in the area, even if they don't have a Rigidbody
+        Collider[] colliders = Physics.OverlapSphere(transform.position, scaledRadius);
+
+        foreach (Collider col in colliders)
+        {
+            // Ignore ourselves just in case
+            if (col.gameObject == gameObject) continue;
+
+            // Check if the collider's layer is NOT in the protectedLayers mask
+            if ((protectedLayers.value & (1 << col.gameObject.layer)) == 0)
+            {
+                col.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void OnCollisionEnter(Collision collision)
     {
-        // deactivate everything that is not in the protected layers
-        foreach (var layer in protectedLayers)
+        if ((protectedLayers.value & (1 << collision.gameObject.layer)) == 0)
         {
-            if (collision.gameObject.layer != layer)
-            {
-                collision.gameObject.SetActive(false);
-            }
+            collision.gameObject.SetActive(false);
         }
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        // deactivate everything that is not in the protected layers
-        foreach (var layer in protectedLayers)
+        if ((protectedLayers.value & (1 << other.gameObject.layer)) == 0)
         {
-            if (other.gameObject.layer != layer)
-            {
-                other.gameObject.SetActive(false);
-            }
+            other.gameObject.SetActive(false);
         }
     }
-
 }
